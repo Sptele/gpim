3095cd60169817f8f6cd319ea47dfb24318e4344
Sptele <gautamkhaj07@gmail.com>
2026-02-02 01:44:58 -0800

    !!!

--- gpim/progmem.h
+++ gpim/progmem.h
@@ -2,20 +2,20 @@
 
 #include <cstdint>
 
-struct DataWriter
+struct DataManager
 {
 	// TODO: read & write function to interact with data.bin
 
 	std::string file_path;
 
-	DataWriter() : file_path("data.bin") {}
-	DataWriter(const std::string& file_path) : file_path(file_path) {}
+	DataManager() : file_path("data.bin") {}
+	DataManager(const std::string& file_path) : file_path(file_path) {}
 };
 
 
 struct ProgramMemory
 {
-	uint32_t PC = 0;
+	uint32_t PC = 0; // TODO: PC should be a pointer (uint32_t*) that literally stores the address of some word
 	uint32_t registers[32] = {};
 	uint32_t r_HI = 0;
 	uint32_t r_LO = 0;
